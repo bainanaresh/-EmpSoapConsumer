4dbe6f21fbc77945ecb31eb1aed7b62b344b23b0
bainanaresh <you@example.com>
2020-02-07 14:41:29 +0530

    soap consumer added here

---
+++ src/main/java/com/baina/EmpSoap2Consumer1Application.java
@@ -0,0 +1,13 @@
+package com.baina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpSoap2Consumer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpSoap2Consumer1Application.class, args);
+	}
+
+}
